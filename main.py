0c336c0573ea35a83603d412841f25cbc1781400
TINOYA <tinoyaalioglybasketball@mail.ru>
2023-11-12 17:41:18 +0300

    ZAR

--- main.py
+++ main.py
@@ -9,19 +9,19 @@ GREY = '\u001b[30;1m'
 END = '\u001b[0m'
 
 
-# for i in range(6):
-#     if i==0:
-#         print(f'{BLUE}{"  "*4} {WHITE}{"  "*4} {RED}{"  "*4}{END}')
-#     elif i==1:
-#         print(f'{BLUE}{"  " * (2+2*i)} {WHITE}{"  " * (2+2*i)} {RED}{"  " *(2+2*i)}{END}')
-#     elif i==2:
-#         print(f'{BLUE}{"  "* (8- (2*i))} {WHITE}{"  "*(8- (2*i))} {RED}{"  "*(8- (2*i))}{END}')
-#     elif i==3:
-#         print(f'{BLUE}{"  "*(10- (2*i))} {WHITE}{"  "*(10- (2*i))} {RED}{"  "*(10- (2*i))}{END}')
-#     elif i==4:
-#         print(f'{BLUE}{"  "*(12- (2*i))} {WHITE}{"  "*(12- (2*i))} {RED}{"  "*(12- (2*i))}{END}')
-#     else :
-#         print(f'{BLUE}{"  "*(14- (2*i))} {WHITE}{"  "*(14- (2*i))} {RED}{"  "*(14-(2*i))}{END}')
+for i in range(6):
+    if i==0:
+        print(f'{BLUE}{"  "*4} {WHITE}{"  "*4} {RED}{"  "*4}{END}')
+    elif i==1:
+        print(f'{BLUE}{"  " * (2+2*i)} {WHITE}{"  " * (2+2*i)} {RED}{"  " *(2+2*i)}{END}')
+    elif i==2:
+        print(f'{BLUE}{"  "* (8- (2*i))} {WHITE}{"  "*(8- (2*i))} {RED}{"  "*(8- (2*i))}{END}')
+    elif i==3:
+        print(f'{BLUE}{"  "*(10- (2*i))} {WHITE}{"  "*(10- (2*i))} {RED}{"  "*(10- (2*i))}{END}')
+    elif i==4:
+        print(f'{BLUE}{"  "*(12- (2*i))} {WHITE}{"  "*(12- (2*i))} {RED}{"  "*(12- (2*i))}{END}')
+    else :
+        print(f'{BLUE}{"  "*(14- (2*i))} {WHITE}{"  "*(14- (2*i))} {RED}{"  "*(14-(2*i))}{END}')
 
         
 
@@ -33,25 +33,25 @@ END = '\u001b[0m'
 
 
 
-# for i in range(9):
-#     if i==0:
-#         print(f'{RED}{"  "*4}{WHITE}{"  "*4}',f'{RED}{"  "*4}{END}')
-#     elif i==1:
-#         print(f'{RED}{"  "*(6-(2*i))}{WHITE}{"  "* (6-(2*i))}',f'{RED}{"  "*(6-(2*i))}{END}')
-#     elif i==2:
-#         print(f'{RED}{"  "*((8-2*i))}{WHITE}{"  "* (8-(2*i))}',f'{RED}{"  "*(8-(2*i))}{END}')
-#     elif i==3:
-#         print(f'{WHITE}{"  "*((10-2*i))}{RED}{"  "* (10-(2*i))}',f'{WHITE}{"  "*(10-(2*i))}{END}')
-#     elif i==4:
-#         print(f'{WHITE}{"  "*((12-2*i))}{RED}{"  "* (12-(2*i))}',f'{WHITE}{"  "*(12-(2*i))}{END}')
-#     elif i==5:
-#         print(f'{WHITE}{"  "*((14-2*i))}{RED}{"  "* (14-(2*i))}',f'{WHITE}{"  "*(14-(2*i))}{END}')
-#     elif i==6:
-#          print(f'{RED}{"  "*((16-2*i))}{WHITE}{"  "* (16-(2*i))}',f'{RED}{"  "*(16-(2*i))}{END}')
-#     elif i==7:
-#          print(f'{RED}{"  "*((18-2*i))}{WHITE}{"  "* (18-(2*i))}',f'{RED}{"  "*(18-(2*i))}{END}')
-#     else:
-#          print(f'{RED}{"  "*((20-2*i))}{WHITE}{"  "* (20-(2*i))}',f'{RED}{"  "*(20-(2*i))}{END}')
+for i in range(9):
+    if i==0:
+        print(f'{RED}{"  "*4}{WHITE}{"  "*4}',f'{RED}{"  "*4}{END}')
+    elif i==1:
+        print(f'{RED}{"  "*(6-(2*i))}{WHITE}{"  "* (6-(2*i))}',f'{RED}{"  "*(6-(2*i))}{END}')
+    elif i==2:
+        print(f'{RED}{"  "*((8-2*i))}{WHITE}{"  "* (8-(2*i))}',f'{RED}{"  "*(8-(2*i))}{END}')
+    elif i==3:
+        print(f'{WHITE}{"  "*((10-2*i))}{RED}{"  "* (10-(2*i))}',f'{WHITE}{"  "*(10-(2*i))}{END}')
+    elif i==4:
+        print(f'{WHITE}{"  "*((12-2*i))}{RED}{"  "* (12-(2*i))}',f'{WHITE}{"  "*(12-(2*i))}{END}')
+    elif i==5:
+        print(f'{WHITE}{"  "*((14-2*i))}{RED}{"  "* (14-(2*i))}',f'{WHITE}{"  "*(14-(2*i))}{END}')
+    elif i==6:
+         print(f'{RED}{"  "*((16-2*i))}{WHITE}{"  "* (16-(2*i))}',f'{RED}{"  "*(16-(2*i))}{END}')
+    elif i==7:
+         print(f'{RED}{"  "*((18-2*i))}{WHITE}{"  "* (18-(2*i))}',f'{RED}{"  "*(18-(2*i))}{END}')
+    else:
+         print(f'{RED}{"  "*((20-2*i))}{WHITE}{"  "* (20-(2*i))}',f'{RED}{"  "*(20-(2*i))}{END}')
 
 
 
@@ -66,34 +66,34 @@ END = '\u001b[0m'
 #  ЗАДАНИЕ 3 ГРАФИК ФУНКЦИИ x^2
 
 
-# COMA = '@'
-# SPACE= "*"
+COMA = '@'
+SPACE= "*"
 
-# if __name__ == '__main__':
+if __name__ == '__main__':
  
     
-#     for i in range(10, 0, -1):
-#         if i==10:
-#             print(i**2,f'{SPACE*18}{COMA*2}')
-#         elif i==9:
-#             print(i**2,f'{SPACE*17}{COMA*2}{SPACE*2}')
-#         elif i==8:
-#             print(i**2,f'{SPACE*15}{COMA*2}{SPACE*4}')
-#         elif i==7:
-#             print(i**2,f'{SPACE*13}{COMA*2}{SPACE*6}')
-#         elif i==6:
-#             print(i**2,f'{SPACE*11}{COMA*2}{SPACE*8}')
-#         elif i==5:
-#             print(i**2,f'{SPACE*9}{COMA*2}{SPACE*10}')
-#         elif i==4:
-#             print(i**2,f'{SPACE*6}{COMA*2}{SPACE*13}')
-#         elif i==3:
-#             print(i**2,f'{SPACE*5}{COMA*2}{SPACE*15}')
-#         elif i==2:
-#             print(i**2,f'{SPACE*3}{COMA*2}{SPACE*17}')
-#         else:
-#             print(i**2,f'{SPACE}{COMA*2}{SPACE*19}')
-#     print(0," ",1,2,3,4,5,6,7,8,9,10)
+    for i in range(10, 0, -1):
+        if i==10:
+            print(i**2,f'{SPACE*18}{COMA*2}')
+        elif i==9:
+            print(i**2,f'{SPACE*17}{COMA*2}{SPACE*2}')
+        elif i==8:
+            print(i**2,f'{SPACE*15}{COMA*2}{SPACE*4}')
+        elif i==7:
+            print(i**2,f'{SPACE*13}{COMA*2}{SPACE*6}')
+        elif i==6:
+            print(i**2,f'{SPACE*11}{COMA*2}{SPACE*8}')
+        elif i==5:
+            print(i**2,f'{SPACE*9}{COMA*2}{SPACE*10}')
+        elif i==4:
+            print(i**2,f'{SPACE*6}{COMA*2}{SPACE*13}')
+        elif i==3:
+            print(i**2,f'{SPACE*5}{COMA*2}{SPACE*15}')
+        elif i==2:
+            print(i**2,f'{SPACE*3}{COMA*2}{SPACE*17}')
+        else:
+            print(i**2,f'{SPACE}{COMA*2}{SPACE*19}')
+    print(0," ",1,2,3,4,5,6,7,8,9,10)
 
 
 
@@ -109,58 +109,58 @@ END = '\u001b[0m'
 
 
 
-# with open('sequence.txt') as fh:
-#     paper = []
-#     for chislo in fh:
-#         if chislo!=0:
-#             paper.append(float(chislo))
-#             print (paper)
-# print ('Количество чисел меньше и больше 0:', len(paper))
+with open('sequence.txt') as fh:
+    paper = []
+    for chislo in fh:
+        if chislo!=0:
+            paper.append(float(chislo))
+            print (paper)
+print ('Количество чисел меньше и больше 0:', len(paper))
 
 
 
-#Используя функцию очищения консольного вывода (```os.system("cls")``` или ```os.system("clear")```), реализовать анимацию из 2-3 кадров.
+# Используя функцию очищения консольного вывода (```os.system("cls")``` или ```os.system("clear")```), реализовать анимацию из 2-3 кадров.
 
 
 
 
-# Доп.задание
-# import time
-# import os
-# clear = lambda : os.system('cls')
-# def draw_stickman():
-#                 print("   O   ")
-#                 print("  /|\  ")
-#                 print("  / \  ") 
-# def drawww_stickman():
-#                 print("   O\   ")
-#                 print("  /|  ")
-#                 print("  / \  ") 
-# def draww_stickman():
-#                 print("   O /  ")
-#                 print("  /|  ")
-#                 print("  / \  ") 
-# def drawn_stickman():
-#         print("   O  |Спасибо за внимание!)")
-#         print("  /|\ /-----")
-#         print("  / \  ") 
+Доп.задание
+import time
+import os
+clear = lambda : os.system('cls')
+def draw_stickman():
+                print("   O   ")
+                print("  /|\  ")
+                print("  / \  ") 
+def drawww_stickman():
+                print("   O\   ")
+                print("  /|  ")
+                print("  / \  ") 
+def draww_stickman():
+                print("   O /  ")
+                print("  /|  ")
+                print("  / \  ") 
+def drawn_stickman():
+        print("   O  |Спасибо за внимание!)")
+        print("  /|\ /-----")
+        print("  / \  ") 
     
-# draw_stickman()
-# time.sleep(1)
-# clear()
-# draww_stickman()
-# time.sleep(0.5)
-# clear()
-# drawww_stickman()
-# time.sleep(0.5)
-# clear()
-# draww_stickman()
-# time.sleep(0.5)
-# clear()
-# drawww_stickman()
-# time.sleep(0.5)
-# clear()
-# drawn_stickman()
+draw_stickman()
+time.sleep(1)
+clear()
+draww_stickman()
+time.sleep(0.5)
+clear()
+drawww_stickman()
+time.sleep(0.5)
+clear()
+draww_stickman()
+time.sleep(0.5)
+clear()
+drawww_stickman()
+time.sleep(0.5)
+clear()
+drawn_stickman()
 
             
 
